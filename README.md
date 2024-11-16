12bff984abf6d122be7b5fe056c27cd34702dade
Parameshwaran <parameshwareshwar81@gmail.com>
2024-11-16 22:07:11 +0530

    my first commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Learninggit
 Hey there i am learning git hub so that i can use it further for my future projects <br>
 so i am updating this so i can see the commit
+
+Changing in vs code
---
+++ index.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+
+
+     printf("Untracked file");
+     printf("change file ---> modified");
+     printf("new file ---> untracked");
+     printf("add ---> staged");
+     printf("commit ---> unchanged");
+     return 0;
+
+}
